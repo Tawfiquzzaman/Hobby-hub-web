318a89529781380855b0e9e09e908f6bfe51561a
mrtopic3050 <mrtopic50@gmail.com>
2025-05-20 21:29:31 +0600

    successfully created routes, dummy navbar and footer

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,34 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router";
+import Root from './pages/Root/Root.jsx';
+import ErrorPage from './pages/Error/ErrorPage.jsx';
+import Home from './pages/Home/Home.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: Root,
+    errorElement: <ErrorPage></ErrorPage>,
+
+    children: [
+      {
+        index: true,
+        path:'/',
+        Component: Home,
+
+      }
+    ]
+
+  },
+]);
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
